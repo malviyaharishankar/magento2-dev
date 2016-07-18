7c17e78c68cb2d87fcb3b4a028f3c7750afe763c
harishankar.malviya <harishankar.malviya@perficient.com>
2016-07-18 16:19:52 +0530

    save all

--- app/code/Feedback/Model/ResourceModel/Feedback.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Perficient\Feedback\Model\ResourceModel;
-class Feedback extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
-{
-    public function _construct()
-    {
-        $this->_init('perficient_feedback', 'id');
-    }
-}
